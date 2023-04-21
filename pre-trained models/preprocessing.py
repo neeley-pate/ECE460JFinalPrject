878b2c2ab7ad8a9087f1c0aceff29f5dd2397f35
dariojimenezp <dario_jimenezp@utexas.edu>
2023-04-21 00:16:31 -0500

    Add vgg16

---
+++ pre-trained models/preprocessing.py
@@ -0,0 +1,85 @@
+import os
+import numpy as np
+import random
+import csv
+from keras.utils import load_img
+from keras.utils import img_to_array
+
+DATASET_PATH = '../datasets/'
+DIGITAL_IMAGES_PATH = 'digital/StableDiffusion/'
+REAL_IMAGES_PATH = 'real-world/'
+PAINTING_IMAGES_PATH = 'Non-digital Artwork/'
+
+def sampleImages(digital_image_samples, real_world_samples, artwork_samples, dimension=(224, 224)):
+    digital_images_files = getImageFiles('digital')
+    real_world_images_files = getImageFiles('real')
+    artwork_images_files = getImageFiles('painting')
+
+
+    digital_sampled_images = random.sample(list(digital_images_files), digital_image_samples)
+    real_sampled_images = random.sample(list(real_world_images_files), real_world_samples)
+    artwork_sampled_images = random.sample(list(artwork_images_files), artwork_samples)
+
+    # concatenate all samples
+    sampled_images = np.concatenate((digital_sampled_images, real_sampled_images, artwork_sampled_images), axis=0)
+
+    images = np.empty((0, dimension[0], dimension[1], 3))
+    for image in sampled_images:
+        img = load_img(DATASET_PATH + image, target_size=dimension)
+        data = img_to_array(img)
+        data = np.expand_dims(data, axis=0)
+        images = np.append(images, data, axis=0)
+
+    # create labels
+    digital_labels = [0 for i in range(digital_image_samples)]
+    real_labels = [1 for i in range(real_world_samples)]
+    artwork_labels = [2 for i in range(artwork_samples)]
+    labels = np.concatenate((digital_labels, real_labels, artwork_labels), axis=0)
+
+    return images, labels
+    
+def getImageFiles(image_class):
+    if image_class == 'digital':
+        files = os.listdir(DATASET_PATH + DIGITAL_IMAGES_PATH)
+        return [DIGITAL_IMAGES_PATH + file for file in files]
+    
+    elif image_class == 'real':
+        dirs = os.listdir(DATASET_PATH + REAL_IMAGES_PATH)
+        real_images_files = np.array([])
+        for dir in dirs:
+            images = os.listdir(DATASET_PATH + REAL_IMAGES_PATH + dir)
+            images = [REAL_IMAGES_PATH + dir + '/' + image for image in images]
+            real_images_files = np.append(real_images_files, images)
+        return real_images_files
+    
+    elif image_class == 'painting':
+        painting_images_dirs = os.listdir(DATASET_PATH + PAINTING_IMAGES_PATH)
+        painting_images_files = np.array([])
+        for dir in painting_images_dirs:
+            images = os.listdir(DATASET_PATH + PAINTING_IMAGES_PATH + dir)
+            images = [PAINTING_IMAGES_PATH + dir + '/' + image for image in images]
+            painting_images_files = np.append(painting_images_files, images)
+        return painting_images_files
+    
+    else:
+        raise ValueError('image_class must be one of digital, real, or painting')
+
+def loadImages(image_class):
+    pass
+
+
+def extract_features_from_images(image_class, offset, num_samples, dimension=(224, 224)):
+    image_files = getImageFiles(image_class)
+    
+    # filename = f'features/{image_class}{offset}.csv'
+    with open('features/' + image_class + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for i in range(offset, offset + num_samples):
+            if(i == len(image_files)):
+                print('Done')
+                break
+            print(i)
+            img = load_img("../datasets/" + image_files[i], target_size=dimension)
+            data = img_to_array(img)
+            flat_data = data.flatten()
+            writer.writerow(flat_data)
---
+++ pre-trained models/vgg16.py
@@ -0,0 +1,58 @@
+import os
+import random
+import numpy as np
+import csv
+
+from keras.applications.vgg16 import VGG16
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import load_img
+from keras.utils import img_to_array
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+from keras.utils import to_categorical
+
+
+def predict(X, Y, model_file):
+    model = load_model(model_file)
+    scores = model.evaluate(X, Y, verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+def train_model(X, Y, model_file):
+    trained_model = VGG16(input_shape = (224, 224, 3), include_top = False, weights = 'imagenet')
+
+    # do not train all layers
+    for layer in trained_model.layers:
+        layer.trainable = False
+
+    # Add final layers that will need to be trained
+    model = Sequential()
+    model.add(trained_model)
+    model.add(Flatten())
+    model.add(Dense(256, activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3, activation = 'sigmoid'))
+
+    # compile the model
+    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+    # split train data into train and validation sets
+    Y = to_categorical(Y, num_classes = 3)
+    x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+    print(Y.shape)
+    # train the model
+    vgghist =  model.fit(x=x_train, y=y_train, batch_size=32, epochs=10, verbose=1, validation_data=(x_val, y_val), shuffle=True, workers=1, use_multiprocessing=True)
+
+    # evaluate the model by calculating accuracy
+    scores = model.evaluate(x_val, y_val, verbose=1)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+    # print accuracy
+    print("Accuracy: %.2f%%" % (scores[1]*100))
+
+    # save the model
+    model.save('vgg-16_fully_trained.h5')
+
